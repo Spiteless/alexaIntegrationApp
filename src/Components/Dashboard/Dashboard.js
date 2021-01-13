438912c3d081a66c0226281ae985b4ce9a4e824c
Trillium <spiteless@gmail.com>
2021-01-12 22:35:11 -0600

    Create component TransactionTable, removed from Dashboard

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,34 +1,11 @@
 import React from 'react';
 import './Dashboard.scss'
-
-const dummy = [{
-    "clientName": "Bob",
-    "date": "1-2-2020",
-}, {
-    "clientName": "Jane",
-    "date": "1-2-2020",
-}, {
-    "clientName": "Joe",
-    "date": "1-5-2020",
-},
-]
+import TransactionTable from '../TransactionTable/TransactionTable.js'
 
 const Dashboard = (props) => {
     return (
         <div>
-            <table>
-                <caption>List of Transactions</caption>
-                <tr>
-                    <th>Name</th>
-                    <th>Date</th>
-                </tr>
-                {dummy.map(session => (
-                    <tr>
-                        <td>{session.clientName}</td>
-                        <td>{session.date}</td>
-                    </tr>
-                ))}
-            </table>
+            <TransactionTable/>
         </div>
     )
 }
---
+++ src/Components/TransactionTable/TransactionTable.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const dummy = [{
+    "clientName": "Bob",
+    "date": "1-2-2020",
+}, {
+    "clientName": "Jane",
+    "date": "1-2-2020",
+}, {
+    "clientName": "Joe",
+    "date": "1-5-2021",
+},
+]
+
+
+const TransactionTable = (props) => {
+    return (
+        <table>
+        <caption>List of Transactions</caption>
+        <tr>
+            <th>Name</th>
+            <th>Date</th>
+        </tr>
+        {dummy.map(session => (
+            <tr>
+                <td>{session.clientName}</td>
+                <td>{session.date}</td>
+            </tr>
+        ))}
+    </table>
+    )
+}
+
+
+export default TransactionTable
